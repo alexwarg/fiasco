73a49418f5c3de0d2589ee02ebe85914b85186e5
Marcus Hähnel <marcus.haehnel@kernkonzept.com>
2019-11-12 20:46:13 +0100

    console: remove disable_all method
    The method to disable all console is not used by anyone but still
    generated code in some circumstances. Remove it.
    
    Change-Id: If9987167b4a6727c8750ab06931fb1904b57ddf3

--- src/drivers/console.cpp
+++ src/drivers/console.cpp
@@ -86,11 +86,6 @@ public:
   { _state &= ~state; }
 
 public:
-  /**
-   * Disables the stdout, stdin, and stderr console.
-   */
-  static void disable_all();
-
   /// stdout for libc glue.
   static Console *stdout;
   /// stderr for libc glue.
@@ -144,14 +139,6 @@ void
 Console::fail()
 {
   _state |= FAILED;
-}
-
-IMPLEMENT
-void Console::disable_all()
-{
-  stdout = 0;
-  stderr = 0;
-  stdin  = 0;
 }
 
 IMPLEMENT
