e7fc8f0f5169650ed97557822090b95327fe2cb1
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-01-03 00:00:00 +0000

    arm: add infrastructure for runtime code patching
    Provide a low-level macro ASM_ALTERNATIVE_ENTRY() on arm that selects
    between two variants of inline assembly depending on the return value
    of a function. This function is called early during boot and the
    respective variant of the assembly is used.
    
    Building upon this low-level facility a high-level "static boolean key"
    is provided. Such a key is evaluated once at boot time. It can be used
    in boolean contexts like any other variable but the actual object code
    will have static branches that are patched depending on the functions
    return value.
    
    Change-Id: I294c58e87b599f3422586ca27d67340fe39254d7

---
+++ src/kern/arm/alternatives.cc
@@ -0,0 +1,29 @@
+
+#include <alternatives.h>
+#include <cstdio>
+
+void
+Alternative_insn::init()
+{
+  extern Alternative_insn const _alt_insns_begin[];
+  extern Alternative_insn const _alt_insns_end[];
+
+  if (0)
+    printf("patching alternative instructions\n");
+
+  if (&_alt_insns_begin[0] == &_alt_insns_end[0])
+    return;
+
+  for (auto *i = _alt_insns_begin; i != _alt_insns_end; ++i)
+    {
+      if (i->probe())
+        {
+          if (0)
+            printf("  replace insn at %p/%d\n", i->disabled_insn(), i->len);
+          i->enable();
+        }
+    }
+
+  // Mem::dsb() already included in Mem_unit::make_coherent_to_pou()
+  Mem::isb();
+}
