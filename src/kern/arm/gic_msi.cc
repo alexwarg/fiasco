f85493332bcae671977ec5cf057609dd1be747da
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-01-24 00:00:00 +0000

    arm: support hardware with ITS page sizes != 4k
    An ITS implementation is not required to implement all page sizes but
    might have a read-only Page_Size field in GITS_BASER<n> with its single
    supported size. We always try to configure 4k pages to keep the
    possible alignment overhead in the buddy allocator low. But if the
    Page_Size field is read back with a different value we use the hardware
    enforced page size.
    
    Change-Id: Id1a9f99692394448bd5a8bcf71f81f050fe72d92

---
+++ src/kern/arm/gic_msi.cc
@@ -0,0 +1,36 @@
+#include <gic_msi.h>
+#include <gic_v3.h>
+#include <boot_alloc.h>
+
+Gic_msi::Gic_msi(Gic_v3 *gic, unsigned num_lpis)
+: _gic(gic)
+{
+  _lpis = Lpi_vec(new Boot_object<Lpi>[num_lpis], num_lpis);
+  for (unsigned i = 0; i < _lpis.size(); i++)
+    _lpis[i].index = i;
+}
+
+Irq_base *
+Gic_msi::irq(Mword pin) const
+{
+  if (pin >= _lpis.size())
+    return nullptr;
+
+  Lpi const &lpi = _lpis[pin];
+  auto g = lock_guard(lpi.lock);
+  return _lpis[pin].irq();
+}
+
+int
+Gic_msi::msg(Mword pin, Unsigned64 src, Irq_mgr::Msi_info *inf)
+{
+  Msi_src msi_src(src);
+  Gic_its *its = _gic->get_its(msi_src.its_num());
+  if (!its)
+    return -L4_err::ERange;
+
+  int err = 0;
+  if (!with_lpi(pin, &Lpi::bind_to_device, its, msi_src.device_id(), inf, err))
+    err = -L4_err::ERange;
+  return err;
+}
