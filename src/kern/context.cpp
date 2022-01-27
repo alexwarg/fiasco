a5e9e11c565e0aecfdca6d0d88f6ba0fae5358fc
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-01-27 07:35:49 +0100

    context: properly synchronize on _running_under_lock
    The _running_under_lock variable acts as a spinlock to synchronize
    helping attempts of other Contexts when they want to acquire a lock
    presumably held by this Context. If the lock was acquired we need to
    make sure that no speculation happened on values that are access after
    this point. Likewise, if the lock is released, any modifications made
    by the caller must be observable to other CPUs before the lock is
    visible as being unlocked.
    
    Fixes ticket #CD-280.
    
    Change-Id: I06df2fab027e6083e93095ca1c697a38fd98c8c1

--- src/kern/context.cpp
+++ src/kern/context.cpp
@@ -1687,7 +1687,10 @@ private:
       if (_s)
         return false; // either running or already trying
 
-      return mp_cas(&_s, Not_running, Trying);
+      bool ret = mp_cas(&_s, Not_running, Trying);
+      if (ret)
+        Mem::mp_acquire();
+      return ret;
     }
 
     /**
@@ -1707,7 +1710,10 @@ private:
       if (access_once(&_s))
         return false;
 
-      return mp_cas(&_s, Not_running, Running);
+      bool ret = mp_cas(&_s, Not_running, Running);
+      if (ret)
+        Mem::mp_acquire();
+      return ret;
     }
 
     /**
@@ -1724,7 +1730,11 @@ private:
      * This method is to be used to abort an unsuccessful attempt to help
      * or after clearing the last lock when running on the home CPU.
      */
-    void reset() { write_now(&_s, Not_running); }
+    void reset()
+    {
+      Mem::mp_release();
+      write_now(&_s, Not_running);
+    }
 
     /**
      * Safe transition from Running to Not_running.
@@ -1735,7 +1745,10 @@ private:
     void preempt()
     {
       if (_s == Running)
-        write_now(&_s, Not_running);
+        {
+          Mem::mp_release();
+          write_now(&_s, Not_running);
+        }
     }
 
     /// Check the current running under lock state.
@@ -1833,8 +1846,6 @@ Context::need_help(Mword const *lock, Mword val)
 {
   if (EXPECT_FALSE(!_running_under_lock.try_to_help()))
     return false;
-
-  Mem::mp_mb();
 
   // double check if the lock is held by us
   if (EXPECT_TRUE(access_once(&_lock_cnt) != 0 && access_once(lock) == val))
