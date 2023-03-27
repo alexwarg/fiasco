415a3265f451e5b42b9656d9b475073c27bd8e7b
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-03-27 00:00:00 +0000

    semaphore: unblock all waiting threads on destroy
    When a semaphore is destroyed, unblock all waiting threads and remove
    them from the _waiting queue because this must not be done after
    returning from Context::schedule() in Semaphore::sys_down() -- the
    Semaphore context might be invalid then.
    
    As the _waiting.lock() is a spinlock, separately grab this lock for
    every member of the _waiting list and release it before potentially
    switching away.
    
    Also, in down(), check if the existence lock is still valid after
    grabbing the _waiting.lock().
    
    See ticket #CD-431.
    
    Change-Id: Ida79520a69b0ddd8b63a2f7e0d3069d7eaaeb3fd

--- src/kern/semaphore.cc
+++ src/kern/semaphore.cc
@@ -44,6 +44,14 @@ public:
         auto g = lock_guard(_waiting.lock());
         if (_queued == 0)
           {
+            // This check is necessary to ensure that a thread does not enqueue
+            // itself into the semaphore´s waiting queue after the semaphore
+            // emptied its waiting queue and unblocked the waiters. Because then
+            // the thread would block forever (assuming it specified an infinite
+            // timeout).
+            if (!existence_lock.valid())
+              return true;
+
             run = false;
             // set fake partner to avoid IPCs to the thread
             // TODO: make really sure that the partner pointer never gets
@@ -79,6 +87,9 @@ public:
                          | Thread_receive_wait
     };
 
+    // Reset IPC error -- Semaphore::destroy() could set it.
+    const_cast<Utcb*>(utcb)->error = L4_error::None;
+
     down(c_thread);
 
     IPC_timeout timeout;
@@ -107,7 +118,38 @@ public:
                                                       : L4_error::R_timeout);
       }
 
-    return commit_result(0);
+    // An IPC error might have been set by Semaphore::destroy() to indicate that
+    // the reason for unblocking the waiting thread was not the completion of the
+    // down operation, but the destruction of the semaphore.
+    if (utcb->error.ok())
+      return commit_result(0);
+    else
+      return L4_msg_tag(0, 0, L4_msg_tag::Error, 0);
+  }
+
+  void destroy(Kobject ***reap_list) override
+  {
+    Irq::destroy(reap_list);
+
+    for (;;)
+      {
+        Thread *t;
+          {
+            auto g = lock_guard(_waiting.lock());
+            Prio_list_elem *f = _waiting.first();
+            if (!f)
+              break;
+
+            _waiting.dequeue(f);
+            t = static_cast<Thread*>(Sender::cast(f));
+            // Do not reset t´s partner because t still has Thread_receive_wait
+            // set. The fake partner avoids IPCs to that thread.
+            t->set_wait_queue(0);
+            t->utcb().access(true)->error = L4_error::Not_existent;
+          }
+
+        t->activate();
+      }
   }
 
   L4_msg_tag kinvoke(L4_obj_ref, L4_fpage::Rights rights, Syscall_frame *f,
