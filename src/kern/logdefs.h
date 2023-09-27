f989df903c913541085659d4a471c93879f836ee
Diego Dias <diego.dias@kernkonzept.com>
2023-09-27 17:49:02 +0200

    do not name the variable argument in variadic macros
    When compiling with -pedantic, the compiler warns that ISO C++
    does not permit named variadic macros (-Wvariadic-macros). This
    change refactors the code to avoid such warnings.
    
    Change-Id: Ie04815220cd23a57d801cf5badc22565329809cb

--- src/kern/logdefs.h
+++ src/kern/logdefs.h
@@ -21,8 +21,8 @@
 
 #if !defined(CONFIG_JDB)
 
-#define LOG_TRACE_COND(name, sc, ctx, fmt, cond, code...) do { } while (0)
-#define LOG_TRACE(name, sc, ctx, fmt, code...) do { } while (0)
+#define LOG_TRACE_COND(name, sc, ctx, fmt, cond, ...) do { } while (0)
+#define LOG_TRACE(name, sc, ctx, fmt, ...)     do { } while (0)
 #define LOG_CONTEXT_SWITCH                     do { } while (0)
 #define LOG_TRAP                               do { } while (0)
 #define LOG_TRAP_N(n)                          do { } while (0)
@@ -38,19 +38,19 @@
 #include "jdb_tbuf.h"
 #include "processor.h"
 
-#define LOG_TRACE_COND(name, sc, ctx, fmt, cond, code...)               \
+#define LOG_TRACE_COND(name, sc, ctx, fmt, cond, ...)                   \
   BEGIN_LOG_EVENT(name, sc, fmt)                                        \
     if (cond)                                                           \
       {                                                                 \
         fmt *l = Jdb_tbuf::new_entry<fmt>();                            \
         l->set_global(__do_log__, ctx, Proc::program_counter());        \
-        {code;}                                                         \
+        {__VA_ARGS__;}                                                  \
         Jdb_tbuf::commit_entry(l);                                      \
       }                                                                 \
   END_LOG_EVENT
 
-#define LOG_TRACE(name, sc, ctx, fmt, code...)                          \
-  LOG_TRACE_COND(name, sc, ctx, fmt, true, code)
+#define LOG_TRACE(name, sc, ctx, fmt, ...)                              \
+  LOG_TRACE_COND(name, sc, ctx, fmt, true, __VA_ARGS__)
 
 #define LOG_CONTEXT_SWITCH                                              \
   LOG_TRACE("Context switch", "csw", this, Tb_entry_ctx_sw,             \
