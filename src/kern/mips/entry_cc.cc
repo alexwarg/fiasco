9cfcdeaaa12bae0e9a72d96711485854c1abecad
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2023-11-06 00:00:00 +0000

    mips: use correct vCPU entry sp
    The vCPU kernel mode entry stack pointer is sampled only at the switch
    from vCPU user to kernel mode. Use the sampled value instead of the
    current one to exhibit the same behaviour as on amd64, arm(64) and x86.
    
    Change-Id: I906146e19add0c74ffe56a5926222f616b3590c8

--- src/kern/mips/entry_cc.cc
+++ src/kern/mips/entry_cc.cc
@@ -11,7 +11,7 @@ void leave_by_vcpu_upcall()
   c->regs()->r[0] = 0; // reset continuation
   Vcpu_state *vcpu = c->vcpu_state().access();
   vcpu->_regs.s = *nonull_static_cast<Trap_state*>(c->regs());
-  Entry::vcpu_return_to_kernel(c, vcpu->_entry_ip, vcpu->_entry_sp, c->vcpu_state().usr().get());
+  Entry::vcpu_return_to_kernel(c, vcpu->_entry_ip, vcpu->_sp, c->vcpu_state().usr().get());
 }
 
 
