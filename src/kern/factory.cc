056d68a2030ebd657c2892d65d33dbb801a57a63
Alexander Warg <alex@wargs.de>
2024-03-18 19:31:05 +0100

    factory: cpp -> h, cc
    Change-Id: I87fbd312649d105dc3f9bde47c3cf654dd5176a3

--- src/kern/factory.cc
+++ src/kern/factory.cc
@@ -1,90 +1,22 @@
-INTERFACE:
-
-#include "fiasco_defs.h"
-#include "ram_quota.h"
-#include "slab_cache.h"
-#include "kobject_helper.h"
-
-class Factory : public Ram_quota, public Kobject_h<Factory>
-{
-  typedef Slab_cache Self_alloc;
-};
-
-//---------------------------------------------------------------------------
-IMPLEMENTATION:
+#include "factory.h"
 
 #include "kmem_slab.h"
 #include "static_init.h"
-#include "l4_buf_iter.h"
-#include "l4_types.h"
-#include "irq.h" // for backward compat
-#include "map_util.h"
 #include "logdefs.h"
-#include "entry_frame.h"
-#include "task.h"
+#include "l4_buf_iter.h"
+
 
 JDB_DEFINE_TYPENAME(Factory, "\033[33;1mFactory\033[m");
 
 static Factory _root_factory INIT_PRIORITY(ROOT_FACTORY_INIT_PRIO);
-
-PUBLIC inline
-Factory::Factory()
-  : Ram_quota()
-{}
-
-PRIVATE inline
-Factory::Factory(Ram_quota *q, Mword max)
-  : Ram_quota(q, max)
-{}
-
-
 static Kmem_slab_t<Factory> _factory_allocator("Factory");
 
-PRIVATE static
 Factory::Self_alloc *
 Factory::allocator()
-{ return _factory_allocator.slab(); }
-
-PUBLIC static inline
-Factory * FIASCO_PURE
-Factory::root()
-{ return nonull_static_cast<Factory*>(Ram_quota::root); }
-
-PUBLIC
-void
-Factory::destroy(Kobject ***rl) override
 {
-  Kobject::destroy(rl);
-  take_and_invalidate();
+  return _factory_allocator.slab();
 }
 
-PUBLIC inline
-bool
-Factory::put() override
-{
-  return Ram_quota::put();
-}
-
-PUBLIC inline NEEDS[Factory::Factory]
-Factory *
-Factory::create_factory(Mword max)
-{
-  if (!check_max(max))
-    return 0;
-
-  Auto_quota<Ram_quota> q(this, sizeof(Factory) + max);
-  if (EXPECT_FALSE(!q))
-    return 0;
-
-  void *nq = allocator()->alloc();
-  if (EXPECT_FALSE(!nq))
-    return 0;
-
-  q.release();
-  return new (nq) Factory(this, max);
-}
-
-PUBLIC
 void Factory::operator delete (void *_f)
 {
   Factory *f = (Factory*)_f;
@@ -102,7 +34,6 @@ void Factory::operator delete (void *_f)
     p->free(sizeof(Factory) + limit);
 }
 
-PRIVATE
 L4_msg_tag
 Factory::map_obj(Kobject_iface *o, Cap_index cap, Task *_c_space,
                  Obj_space *o_space, Utcb const *utcb)
@@ -132,7 +63,6 @@ Factory::map_obj(Kobject_iface *o, Cap_index cap, Task *_c_space,
   return commit_result(0, 0, 1);
 }
 
-PUBLIC
 L4_msg_tag
 Factory::kinvoke(L4_obj_ref ref, L4_fpage::Rights rights, Syscall_frame *f,
                  Utcb const *utcb, Utcb *utcb_out)
@@ -204,43 +134,3 @@ register_factory()
 }
 }
 
-// ------------------------------------------------------------------------
-INTERFACE [debug]:
-
-#include "tb_entry.h"
-
-EXTENSION class Factory
-{
-private:
-  struct Log_entry : public Tb_entry
-  {
-    Smword op;
-    Cap_index buffer;
-    Mword id;
-    Mword ram;
-    Mword newo;
-    void print(String_buffer *buf) const;
-  };
-};
-
-// ------------------------------------------------------------------------
-IMPLEMENTATION [debug]:
-
-#include "string_buffer.h"
-
-IMPLEMENT
-void
-Factory::Log_entry::print(String_buffer *buf) const
-{
-  static char const *const ops[] =
-  { /*   0 */ "gate", "irq", 0, 0, 0, 0, 0, 0,
-    /*  -8 */ 0, 0, 0, "task", "thread", 0, 0, "factory",
-    /* -16 */ "vm", "dmaspace", "irqsender", 0, "sem" };
-  char const *_op = -op < (int)(sizeof(ops) / sizeof(ops[0]))
-    ? ops[-op] : "invalid op";
-  if (!_op)
-    _op = "(nan)";
-
-  buf->printf("factory=%lx [%s] new=%lx cap=[C:%lx] ram=%lx",
-              id, _op, newo, cxx::int_value<Cap_index>(buffer), ram);
-}
---
+++ src/kern/factory.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "fiasco_defs.h"
+#include "ram_quota.h"
+#include "slab_cache.h"
+#include "kobject_helper.h"
+
+#if defined (CONFIG_JDB)
+
+#include "tb_entry.h"
+#include "string_buffer.h"
+
+#endif // CONFIG_JDB
+
+class Factory : public Ram_quota, public Kobject_h<Factory>
+{
+  typedef Slab_cache Self_alloc;
+
+public:
+  Factory() = default;
+  Factory(Ram_quota *q, Mword max) noexcept : Ram_quota(q, max) {}
+
+  void operator delete (void *_f);
+
+  [[gnu::pure]]
+  static Factory *root()
+  { return nonull_static_cast<Factory*>(Ram_quota::root); }
+
+  void destroy(Kobject ***rl) override
+  {
+    Kobject::destroy(rl);
+    take_and_invalidate();
+  }
+
+  bool put() override
+  {
+    return Ram_quota::put();
+  }
+
+  Factory *create_factory(Mword max)
+  {
+    if (!check_max(max))
+      return 0;
+
+    Auto_quota<Ram_quota> q(this, sizeof(Factory) + max);
+    if (EXPECT_FALSE(!q))
+      return 0;
+
+    void *nq = allocator()->alloc();
+    if (EXPECT_FALSE(!nq))
+      return 0;
+
+    q.release();
+    return new (nq) Factory(this, max);
+  }
+
+  L4_msg_tag kinvoke(L4_obj_ref ref, L4_fpage::Rights rights, Syscall_frame *f,
+                     Utcb const *utcb, Utcb *utcb_out);
+
+
+private:
+  static Self_alloc *allocator();
+
+  L4_msg_tag map_obj(Kobject_iface *o, Cap_index cap, Task *_c_space,
+                     Obj_space *o_space, Utcb const *utcb);
+
+#if defined (CONFIG_JDB)
+  struct Log_entry : public Tb_entry
+  {
+    Smword op;
+    Cap_index buffer;
+    Mword id;
+    Mword ram;
+    Mword newo;
+    void print(String_buffer *buf) const
+    {
+      static char const *const ops[] =
+      { /*   0 */ "gate", "irq", 0, 0, 0, 0, 0, 0,
+        /*  -8 */ 0, 0, 0, "task", "thread", 0, 0, "factory",
+        /* -16 */ "vm", "dmaspace", "irqsender", 0, "sem" };
+      char const *_op = -op < (int)(sizeof(ops) / sizeof(ops[0]))
+        ? ops[-op] : "invalid op";
+      if (!_op)
+        _op = "(nan)";
+
+      buf->printf("factory=%lx [%s] new=%lx cap=[C:%lx] ram=%lx",
+                  id, _op, newo, cxx::int_value<Cap_index>(buffer), ram);
+    }
+  };
+#endif // CONFIG_JDB
+};
+
