63726a77e0557153c601248d253c77cb5cd7e4bb
Alexander Warg <alex@wargs.de>
2024-03-19 18:28:54 +0100

    banner: cpp -> h, cc
    Change-Id: I3c7b04fcca6b7e4796aa7a9af906727c4c9f3b61

--- src/kern/banner.cc
+++ src/kern/banner.cc
@@ -1,25 +1,21 @@
-INTERFACE:
-
-#include "initcalls.h"
-
-class Banner
-{
-};
-
-
-IMPLEMENTATION:
+#include "banner.h"
 
 #include <cstdio>
 #include "config.h"
+#include "initcalls.h"
 
-PUBLIC static FIASCO_INIT
-void
-Banner::init()
+extern char _initkip_start[];
+
+namespace Banner {
+
+FIASCO_INIT void
+init()
 {
-  extern char _initkip_start[];
   printf("\n%s\n", _initkip_start);
   if (Config::kernel_warn_config_string && *Config::kernel_warn_config_string)
     printf("\033[31mPerformance-critical config option(s) detected:\n"
 	   "%s\033[m", Config::kernel_warn_config_string);
   putchar('\n');
+}
+
 }
