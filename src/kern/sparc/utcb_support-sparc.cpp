360e41e38d7af9e217398548dc20fb1754378cf6
Alexander Warg <alex@wargs.de>
2024-03-22 23:48:57 +0100

    drop PPC32, and sarc support (in kern)
    Change-Id: I3c0aee00d0fe030cb311073624335da57ebc0fa0

--- src/kern/sparc/utcb_support-sparc.cpp
+++
@@ -1,9 +0,0 @@
-//-------------------------------------------------------------------------
-IMPLEMENTATION [sparc]:
-
-IMPLEMENT inline
-void
-Utcb_support::current(User<Utcb>::Ptr const &utcb)
-{
-  (void)utcb;
-}
