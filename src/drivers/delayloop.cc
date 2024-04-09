8cad4de26912ca50e13e400c3ad6e984ef1dda46
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-04-09 17:32:24 +0200

    delayloop: improve reliability on certain platforms
    Delay::delay() is used wait for a certain period of time when there is
    no timer tick is available -- in JDB. Delay::measure() executes a loop
    and determines the number of turns with reading from the KIP clock until
    the clock advances by one millisecond.
    
    It is important that Delay::measure() executes code similar to
    Delay::delay() to have a reasonable accuracy. Add barriers to prevent
    the compiler from using conditional return statements within the
    measurement loop.
    
    For example on ARM32, without this change, the code actually waits for
    less than 1/4th comparing to the intended period.
    
    Change-Id: Ib7e2fb4eec5dc64a4635075b2119e69e20e2cb48

--- src/drivers/delayloop.cc
+++ src/drivers/delayloop.cc
@@ -1,6 +1,7 @@
 #include <delayloop.h>
 
 #include <kip.h>
+#include <mem.h>
 #include <processor.h>
 #include <timer.h>
 
@@ -16,12 +17,16 @@ static unsigned measure()
   Timer::update_timer(t + 1000); // 1ms
   while (t == (t1 = k->clock()))
     Proc::pause();
-  Timer::update_timer(k->clock() + 1000); // 1ms
+  Timer::update_timer(t1 + 1000); // 1ms
+
+  // Execute code as similar to delay() as possible to get a reliable count.
+  Mem::barrier();
   while (t1 == k->clock())
     {
       ++count;
       Proc::pause();
     }
+  Mem::barrier();
 
   return count;
 }
@@ -36,8 +41,12 @@ Delay::init()
 }
 
 /**
- * Hint: ms is actually the timer granularity, which
- *       currently happens to be milliseconds
+ * Wait for a certain amount of time.
+ *
+ * \param ms  The number of milliseconds to wait for.
+ *
+ * Can be used in the kernel debugger while no timer tick is available. Don't
+ * expect 100% accurate delays here.
  */
 void
 Delay::delay(unsigned ms)
@@ -45,11 +54,17 @@ Delay::delay(unsigned ms)
   Kip *k = Kip::k();
   while (ms--)
     {
+      // 'count' was determined by waiting for the KIP counter to change from
+      // one value to the next value. Hence, 'count' has KIP counter granularity
+      // -- which is currently 1ms.
       unsigned c = cyc_count;
+
+      Mem::barrier();
       while (c--)
         {
           static_cast<void>(k->clock());
           Proc::pause();
         }
+      Mem::barrier();
     }
 }
