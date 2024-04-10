8e22b6a3f0855e93b8f8707d1d64a7adf5187600
Alexander Warg <alex@wargs.de>
2024-04-10 19:23:02 +0200

    arm/smc_call: cpp -> h

--- src/kern/arm/64/smc_call.h
+++ src/kern/arm/64/smc_call.h
@@ -1,16 +1,4 @@
-INTERFACE [arm && 32bit]:
-
-#define FIASCO_ARM_SMC_CALL_ASM_OPERANDS \
-    : "=r" (r0), "=r" (r1), "=r" (r2), "=r" (r3) \
-    : "0" (r0), "1" (r1), "2" (r2), "3" (r3), \
-      "r" (r4), "r" (r5), "r" (r6), "r" (r7) \
-    : "memory"
-
-#define FIASCO_ARM_ASM_REG(n) asm("r" # n)
-
-#define FIASCO_ARM_ARCH_EXTENSION_SEC ".arch_extension sec\n"
-
-INTERFACE [arm && 64bit]:
+#pragma once
 
 #define FIASCO_ARM_SMC_CALL_ASM_OPERANDS \
     : "=r" (r0), "=r" (r1), "=r" (r2), "=r" (r3), \
