424b3476274611497a7f302d0b407725549231ec
Alexander Warg <alex@wargs.de>
2024-04-10 18:28:02 +0200

    mips/alternatives: cpp -> h, cc

---
+++ src/kern/mips/alternatives.cc
@@ -0,0 +1,59 @@
+
+#include <alternatives.h>
+
+#include <cstdio>
+#include <cstring>
+#include "asm_mips.h"
+
+
+inline void
+Alternative_insn::replace() const
+{
+  Unsigned32 *orig_insn = orig_code();
+  Unsigned32 const *alt_insn = this->alt_insn();
+  memcpy(orig_insn, alt_insn, r_len);
+  if (r_len < total_len)
+    memset((char *)orig_insn + r_len, 0, total_len - r_len);
+
+  // sync insn cache, this code does not use synci_step but uses 4byte steps
+  for (unsigned i = 0; i <= total_len / 4; ++i)
+    asm volatile ("synci %0" : : "R"(orig_insn[i]));
+
+  Mword dummy;
+  asm volatile ("sync; " ASM_LA " %0, 1f; jr.hb %0; nop; 1: nop;" : "=r"(dummy));
+}
+
+void
+Alternative_insn::handle_alternatives(unsigned features)
+{
+  extern Alternative_insn const _alt_insns_begin[];
+  extern Alternative_insn const _alt_insns_end[];
+
+  if (0)
+    printf("patching alternative instructions for feature: %x\n", features);
+
+  for (auto *i = _alt_insns_begin; i != _alt_insns_end; ++i)
+    {
+      if (i->feature & features)
+        {
+          if (0)
+            printf("  replace insn at %p %08x -> %08x\n",
+                   i->orig_code(), *i->orig_code(), *i->alt_insn());
+          i->replace();
+        }
+    }
+
+  // finally clear all instruction hazards
+  asm volatile (
+      ".set push\n\t"
+      ".set noreorder\n\t"
+      ".set noat\n\t"
+      "sync\n\t"
+      ASM_LA " $at, 1f\n\t"
+      "jr.hb $at\n\t"
+      "  nop\n\t"
+      "1:\n\t"
+      ".set pop"
+      );
+}
+
--- src/kern/mips/alternatives.h
+++ src/kern/mips/alternatives.h
@@ -1,4 +1,4 @@
-INTERFACE:
+#pragma once
 
 #include <types.h>
 
@@ -21,6 +21,22 @@ struct Alternative_insn
   Unsigned32 feature; ///< feature bit that enables the alternative
   Unsigned8 total_len; ///< Total number of bytes in the code
   Unsigned8 r_len;     ///< Length of this replacement in bytes
+                       //
+
+  Unsigned32 *orig_code() const
+  {
+    return reinterpret_cast<Unsigned32*>(reinterpret_cast<Address>(this) + orig);
+  }
+
+  Unsigned32 const *alt_insn() const
+  {
+    return reinterpret_cast<Unsigned32*>(reinterpret_cast<Address>(this) + alt);
+  }
+
+  static void handle_alternatives(unsigned features);
+
+private:
+  void replace() const;
 };
 
 #define ASM_ALTERNATIVE_ENTRY(feature, idx) \
@@ -47,77 +63,4 @@ struct Alternative_insn
         "8991:\n\t"                                       \
         ".popsection                   \n\t"
 
-IMPLEMENTATION:
-
-#include <cstdio>
-#include <cstring>
-#include "asm_mips.h"
-
-PUBLIC inline
-Unsigned32 *
-Alternative_insn::orig_code() const
-{
-  return reinterpret_cast<Unsigned32*>(reinterpret_cast<Address>(this) + orig);
-}
-
-PUBLIC inline
-Unsigned32 const *
-Alternative_insn::alt_insn() const
-{
-  return reinterpret_cast<Unsigned32*>(reinterpret_cast<Address>(this) + alt);
-}
-
-
-PRIVATE inline NOEXPORT
-void
-Alternative_insn::replace() const
-{
-  Unsigned32 *orig_insn = orig_code();
-  Unsigned32 const *alt_insn = this->alt_insn();
-  memcpy(orig_insn, alt_insn, r_len);
-  if (r_len < total_len)
-    memset((char *)orig_insn + r_len, 0, total_len - r_len);
-
-  // sync insn cache, this code does not use synci_step but uses 4byte steps
-  for (unsigned i = 0; i <= total_len / 4; ++i)
-    asm volatile ("synci %0" : : "R"(orig_insn[i]));
-
-  Mword dummy;
-  asm volatile ("sync; " ASM_LA " %0, 1f; jr.hb %0; nop; 1: nop;" : "=r"(dummy));
-}
-
-PUBLIC static
-void
-Alternative_insn::handle_alternatives(unsigned features)
-{
-  extern Alternative_insn const _alt_insns_begin[];
-  extern Alternative_insn const _alt_insns_end[];
-
-  if (0)
-    printf("patching alternative instructions for feature: %x\n", features);
-
-  for (auto *i = _alt_insns_begin; i != _alt_insns_end; ++i)
-    {
-      if (i->feature & features)
-        {
-          if (0)
-            printf("  replace insn at %p %08x -> %08x\n",
-                   i->orig_code(), *i->orig_code(), *i->alt_insn());
-          i->replace();
-        }
-    }
-
-  // finally clear all instruction hazards
-  asm volatile (
-      ".set push\n\t"
-      ".set noreorder\n\t"
-      ".set noat\n\t"
-      "sync\n\t"
-      ASM_LA " $at, 1f\n\t"
-      "jr.hb $at\n\t"
-      "  nop\n\t"
-      "1:\n\t"
-      ".set pop"
-      );
-}
 
