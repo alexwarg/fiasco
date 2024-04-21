0a35e8bc31ef925dc6abb171880bbc28bd1bc880
Alexander Warg <alex@wargs.de>
2024-04-21 11:49:14 +0200

    ia32/timer_irq: cpp -> cc

--- src/kern/ia32/timer_irq.cc
+++ src/kern/ia32/timer_irq.cc
@@ -1,5 +1,3 @@
-//---------------------------------------------------------------------------
-IMPLEMENTATION[ia32 || amd64]:
 
 #include <config.h>
 #include <globals.h>
@@ -14,17 +12,17 @@ IMPLEMENTATION[ia32 || amd64]:
     Checks if something related to debugging is to do. After returning
     from this function, the real timer interrupt handler is called.
  */
-extern "C"
-void
-thread_timer_interrupt_slow(void)
+extern "C" void thread_timer_interrupt_slow();
+
+void thread_timer_interrupt_slow()
 {
   if (current_cpu() == Cpu_number::boot_cpu())
     {
       if (Config::watchdog)
-	{
-	  // tell doggy that we are alive
-	  Watchdog::touch();
-	}
+        {
+          // tell doggy that we are alive
+          Watchdog::touch();
+        }
     }
 }
 
