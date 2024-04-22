2a5ab6783b2c303864d5f7276a9093db870a2ddf
Alexander Warg <alex@wargs.de>
2024-04-22 19:11:14 +0200

    ia32/io_apic_remapped: cpp -> h, cc

---
+++ src/kern/ia32/io_apic_remapped.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <io_apic.h>
+
+namespace Intel { class Io_mmu; }
+
+class Io_apic_remapped : public Io_apic
+{
+public:
+  Io_apic_remapped(Unsigned64 phys, unsigned gsi_base,
+                   Intel::Io_mmu *iommu, Unsigned16 src_id)
+  : Io_apic(phys, gsi_base), _iommu(iommu), _src_id(src_id)
+  {}
+
+  bool alloc(Irq_base *irq, Mword pin, bool init = true) override;
+  void unbind(Irq_base *irq) override;
+  int set_mode(Mword pin, Mode mode) override;
+  void set_cpu(Mword pin, Cpu_number cpu) override;
+
+  static bool init_apics();
+
+#ifdef CONFIG_JDB
+  char const *chip_type() const override
+  { return "rIO-APIC"; }
+#endif
+
+private:
+  Intel::Io_mmu *_iommu;
+  Unsigned16 _src_id;
+};
+
+
