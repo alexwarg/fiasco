932822e8f9d43f000361301b0575c6a2f5b0d8a0
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-05-30 18:33:23 +0200

    koptions: make accessor a "const" function
    The return value of Koptions::o does not depend on an external state.
    By declaring the function "const", repeated calls can be optimized away
    by the compiler.
    
    Change-Id: Ied98a2b8a53a89dabeceddbad85d2c1ad291105d

--- src/kern/koptions.h
+++ src/kern/koptions.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "koptions-def.h"
+#include "std_macros.h"
 
 namespace Koptions {
   using namespace L4_kernel_options;
 
-  Options *o();
+  Options *o() FIASCO_CONST;
 }
 
