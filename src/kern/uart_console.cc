27c8bb7838488ab7b2ab69f2b9ddd89a353dc2aa
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-10-28 00:00:00 +0000

    uart_console: do not touch kernel_uart if disabled
    If the serial support is completely disabled (-noserial kernel option),
    the Kernel_uart::uart() singleton object is not constructed. Hence, it
    must not be touched. In fact, if assertions are enabled, doing so leads
    to a panic because the Static_object class checks for the construction.
    
    Change-Id: Ia3a339c1f2d53cc7fef576ef95c7741d3336064c

--- src/kern/uart_console.cc
+++ src/kern/uart_console.cc
@@ -6,12 +6,18 @@
 #include "filter_console.h"
 #include "kernel_console.h"
 #include "kernel_uart.h"
+#include "koptions.h"
 #include "static_init.h"
 #include "irq.h"
 
 
 static void uart_console_init_stage2()
 {
+  // Do not touch Kernel_uart::uart() if serial support is disabled as a whole.
+  // The object won't be constructed in this case.
+  if (Koptions::o()->opt(Koptions::F_noserial))
+    return;
+
   if ((Kernel_uart::uart()->failed()))
     return;
 
