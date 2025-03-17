bb403bb3c5c933af751954420bfceba4caf72b37
Frank Mehnert <frank.mehnert@kernkonzept.com>
2025-03-17 00:00:00 +0000

    kobject_helper: wrap long line to improve code readability
    Change-Id: I4aaafe71c93fb3dd468069c5bbb50117cf839427

--- src/kern/kobject_helper.h
+++ src/kern/kobject_helper.h
@@ -57,7 +57,9 @@ public:
         if (!res.do_switch())
           {
             Thread *t = current_thread();
-            Sender *s = (self.op() & L4_obj_ref::Ipc_open_wait) ? 0 : _sender(t, static_cast<T*>(this));
+            Sender *s = (self.op() & L4_obj_ref::Ipc_open_wait)
+                          ? nullptr
+                          : _sender(t, static_cast<T*>(this));
             t->do_ipc(f->tag(), 0, true, s, f->timeout(), f);
             return;
           }
