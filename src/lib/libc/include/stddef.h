d1ac211d05306685f60995d0ea956e99e1086aba
Frank Mehnert <frank.mehnert@kernkonzept.com>
2025-10-04 19:02:53 +0200

    libc: remove support for ancient gcc versions
    Change-Id: I8287a5e715c72af579f9abd73d6254308e37be5f

--- src/lib/libc/include/stddef.h
+++ src/lib/libc/include/stddef.h
@@ -21,10 +21,6 @@ typedef decltype(nullptr) nullptr_t;
 
 #include <bits/alltypes.h>
 
-#if __GNUC__ > 3
 #define offsetof(type, member) __builtin_offsetof(type, member)
-#else
-#define offsetof(type, member) ((size_t)( (char *)&(((type *)0)->member) - (char *)0 ))
-#endif
 
 #endif
