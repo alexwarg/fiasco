1e54094c5d2448da24b1f15fe0f9bf336e2645a4
Frank Mehnert <frank.mehnert@kernkonzept.com>
2025-10-20 10:16:31 +0200

    always terminate messages shown by WARN() or WARNX() with '\n'
    Messages shown by WARN() or WARNX() shall be terminated with '\n' to
    make sure that the next message starts on a new line.
    
    Change-Id: I0012f2d4649c37a04cc1ab7f3be7315b8aacd56d

--- src/kern/ia32/task_ia32_ldt.cc
+++ src/kern/ia32/task_ia32_ldt.cc
@@ -32,7 +32,7 @@ Task::invoke_arch(L4_msg_tag &tag, Utcb *utcb)
 
           if (entry_number * Cpu::Ldt_entry_size + size > Config::PAGE_SIZE)
             {
-              WARN("set_ldt: LDT size exceeds one page, not supported.");
+              WARN("set_ldt: LDT size exceeds one page, not supported.\n");
               tag = commit_result(-L4_err::EInval);
               return true;
             }
@@ -49,7 +49,7 @@ Task::invoke_arch(L4_msg_tag &tag, Utcb *utcb)
               desc = *reinterpret_cast<Gdt_entry const *>(desc_addr);
               if (desc.unsafe())
                 {
-                  WARN("set_ldt: Bad descriptor.");
+                  WARN("set_ldt: Bad descriptor.\n");
                   tag = commit_result(-L4_err::EInval);
                   return true;
                 }
