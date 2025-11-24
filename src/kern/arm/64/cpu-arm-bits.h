1ca4e27c157297f25c9843133d3315167fcc8f56
Adam Lackorzynski <adam@l4re.org>
2025-11-24 00:00:00 +0000

    arm64: Cpu::phys_bits(): add 56bits (FEAT_D128)
    Change-Id: I6a2c3165ee88d59f51cf941e994151ad4f34a951
    Signed-off-by: Adam Lackorzynski <adam@l4re.org>

--- src/kern/arm/64/cpu-arm-bits.h
+++ src/kern/arm/64/cpu-arm-bits.h
@@ -202,7 +202,7 @@ public:
 
   static unsigned phys_bits()
   {
-    static char const pa_range_bits[16] = { 32, 36, 40, 42, 44, 48, 52 };
+    static char const pa_range_bits[16] = { 32, 36, 40, 42, 44, 48, 52, 56 };
     return pa_range_bits[pa_range()];
   }
 
