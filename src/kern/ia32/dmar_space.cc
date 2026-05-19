ca356a7ce0eeef476a677bb331e6108dae7b8cb9
Alexander Warg <alex@wargs.de>
2026-05-19 19:57:40 +0200

    ia32: dmar_space: cpp -> h, cc
    Change-Id: Ie8f7042019c0ffe41ad339244a5f4d7951704ca2

--- src/kern/ia32/dmar_space.cc
+++ src/kern/ia32/dmar_space.cc
@@ -1,160 +1,14 @@
-INTERFACE [iommu]:
+#include <dmar_space.h>
 
-#include "task.h"
-#include "ptab_base.h"
-#include "bitmap.h"
+#include <task.h>
+#include <ptab_base.h>
+#include <bitmap.h>
 
-class Dmar_space :
-  public cxx::Dyn_castable<Dmar_space, Task>
-{
-private:
-  template<typename T>
-  static void clean_dcache(T *p)
-  { Mem_unit::clean_dcache(p, p + 1); }
-
-  class Dmar_ptr
-  {
-  public:
-    struct Dmar_ptr_val
-    {
-      Unsigned64 v;
-      Dmar_ptr_val() = default;
-      Dmar_ptr_val(Unsigned64 v) : v(v) {}
-      CXX_BITFIELD_MEMBER(0, 1, present, v);
-    };
-
-    Dmar_ptr_val *e;
-
-  public:
-    typedef Mem_space::Attr Attr;
-
-    unsigned char level;
-    Dmar_ptr() = default;
-    Dmar_ptr(Unsigned64 *e, unsigned char l)
-    : e(reinterpret_cast<Dmar_ptr_val*>(e)), level(l) {}
-
-    bool is_valid() const { return e->present(); }
-    bool is_leaf() const
-    { return (level == Dmar_pt::Depth) || (e->v & (1 << 7)); }
-    Unsigned64 next_level() const
-    { return cxx::mask_lsb(e->v, Config::PAGE_SHIFT); }
-
-    void set(Unsigned64 v);
-    void clear() { set(0); }
-
-    unsigned char page_order() const;
-    Unsigned64 page_addr() const;
-    Attr attribs() const
-    {
-      typedef L4_fpage::Rights R;
-
-      auto raw = access_once(&e->v);
-
-      R r = R::UR();
-      if (raw & 2) r |= R::W();
-
-      return Attr(r, Page::Type::Normal());
-    }
-
-    bool add_attribs(Page::Attr attr)
-    {
-      typedef L4_fpage::Rights R;
-
-      if (attr.rights & R::W())
-        {
-          auto p = access_once(&e->v);
-          auto o = p;
-          p |= 2;
-          if (o != p)
-            {
-              write_now(&e->v, p);
-              clean_dcache(e);
-              return true;
-            }
-        }
-      return false;
-    }
-
-    void set_next_level(Unsigned64 phys)
-    { set(phys | 3); }
-
-    void write_back_if(bool) const {}
-    static void write_back(void *, void *) {}
-
-    L4_fpage::Rights access_flags() const
-    {
-      return L4_fpage::Rights(0);
-    }
-
-    void del_rights(L4_fpage::Rights r)
-    {
-      if (r & L4_fpage::Rights::W())
-        {
-          auto p = access_once(&e->v);
-          auto o = p & ~(Unsigned64)2;
-          if (o != p)
-            {
-              write_now(&e->v, p);
-              clean_dcache(e);
-            }
-        }
-    }
-
-    /*
-     * WARNING: The VT-d documentation says that the super page bit
-     * WARNING: is ignored in page table entries for 4k pages. However,
-     * WARNING: this is not true. The super page bit must be zero.
-     */
-    void create_page(Phys_mem_addr addr, Page::Attr attr)
-    {
-      typedef L4_fpage::Rights R;
-
-      assert(level <= Dmar_pt::Depth);
-      Unsigned64 r = (level == Dmar_pt::Depth) ? 0 : (Unsigned64)(1<<7);
-      r |= 1; // Read
-      if (attr.rights & R::W()) r |= 2;
-
-      set(cxx::int_value<Phys_mem_addr>(addr) | r);
-    }
-  };
-
-  typedef Ptab::Tupel<Ptab::Traits<Unsigned64, 39, 9, true>,
-                      Ptab::Traits<Unsigned64, 30, 9, true>,
-                      Ptab::Traits<Unsigned64, 21, 9, true>,
-                      Ptab::Traits<Unsigned64, 12, 9, true> >::List Dmar_traits;
-
-  typedef Ptab::Shift<Dmar_traits, 12>::List Dmar_traits_vpn;
-  typedef Ptab::Page_addr_wrap<Page_number, 12> Dmar_va_vpn;
-  typedef Ptab::Base<Dmar_ptr, Dmar_traits_vpn, Dmar_va_vpn, Mem_layout> Dmar_pt;
-
-public:
-  enum { Max_nr_did = 0x10000 };
-  virtual void *debug_dir() const { return (void *)_dmarpt; }
-  static void create_identity_map();
-  static Dmar_pt *identity_map;
-
-  void tlb_flush(bool) override;
-
-private:
-  Dmar_pt *_dmarpt;
-  unsigned long _did;
-
-  static bool _initialized;
-
-  typedef Bitmap<Max_nr_did> Did_map;
-
-  static Did_map *_free_dids;
-  static unsigned _max_did;
-};
-
-// -----------------------------------------------------------
-IMPLEMENTATION [iommu]:
-
-#include "boot_alloc.h"
-#include "intel_iommu.h"
-#include "kmem_slab.h"
-#include "warn.h"
-#include "task_factory_impl.h"
+#include <boot_alloc.h>
+#include <intel_iommu.h>
+#include <kmem_slab.h>
+#include <warn.h>
+#include <task_factory_impl.h>
 
 JDB_DEFINE_TYPENAME(Dmar_space, "DMA");
 
@@ -163,37 +17,12 @@ bool Dmar_space::_initialized;
 Dmar_space::Did_map *Dmar_space::_free_dids;
 unsigned Dmar_space::_max_did;
 
-
-PUBLIC
 Page_number
-Dmar_space::mem_space_map_max_address() const override
+Dmar_space::mem_space_map_max_address() const
 {
   return Page_number(1UL << (Intel::Io_mmu::hw_addr_width - Mem_space::Page_shift));
 }
 
-PUBLIC static inline
-Mword
-Dmar_space::get_root(Dmar_pt *pt, unsigned aw_level)
-{
-  aw_level += 2;
-  if (aw_level == Dmar_pt::Depth + 1)
-    return Mem_layout::pmem_to_phys(pt);
-
-  assert(aw_level <= Dmar_pt::Depth);
-
-  auto i = pt->walk(Mem_space::V_pfn(0), Dmar_pt::Depth - aw_level);
-  assert(i.is_valid());
-  return i.next_level();
-}
-
-PUBLIC inline
-Mword
-Dmar_space::get_root(int aw_level) const
-{
-  return get_root(_dmarpt, aw_level);
-}
-
-PRIVATE static
 unsigned
 Dmar_space::alloc_did()
 {
@@ -206,7 +35,6 @@ Dmar_space::alloc_did()
   return ~0U;
 }
 
-PRIVATE static
 void
 Dmar_space::free_did(unsigned long did)
 {
@@ -214,7 +42,6 @@ Dmar_space::free_did(unsigned long did)
     panic("DMAR: Freeing free DID");
 }
 
-PUBLIC static
 void
 Dmar_space::init(unsigned max_did)
 {
@@ -226,73 +53,6 @@ Dmar_space::init(unsigned max_did)
   _initialized = true;
 }
 
-PUBLIC inline
-bool
-Dmar_space::initialize()
-{
-  void *b;
-
-  if (!_initialized)
-    return false;
-
-  b = Kmem_alloc::allocator()->q_alloc(ram_quota(), Config::page_order());
-  if (EXPECT_FALSE(!b))
-    return false;
-
-  _dmarpt = static_cast<Dmar_pt *>(b);
-  _dmarpt->clear(false);
-
-  /*
-   * Make sure that the very first entry in a page table is valid and
-   * not a super page. This is necessary if the hardware supports
-   * fewer levels than the current software implementation.
-   *
-   * Force allocation of two levels in entry 0, so get_root works
-   */
-  auto i = _dmarpt->walk(Mem_space::V_pfn(0), 2, false,
-                         Kmem_alloc::q_allocator(ram_quota()));
-  if (i.level != 2)
-    {
-      // Got a page-table entry with the wrong level. That happens in the
-      // case of an out-of-memory situation. So free everything we already
-      // allocated and fail.
-      _dmarpt->destroy(Virt_addr(0UL), Virt_addr(~0UL), 0, Dmar_pt::Depth,
-                       Kmem_alloc::q_allocator(ram_quota()));
-      Kmem_alloc::allocator()->q_free(ram_quota(), Config::page_order(), _dmarpt);
-      _dmarpt = 0;
-      return false;
-    }
-
-  return true;
-}
-
-IMPLEMENT inline
-void
-Dmar_space::Dmar_ptr::set(Unsigned64 v)
-{
-  write_consistent(e, Dmar_ptr_val(v));
-  clean_dcache(e);
-}
-
-PUBLIC inline
-unsigned long
-Dmar_space::get_did()
-{
-  // XXX: possibly need a loop here
-  if (_did == 0)
-    {
-      unsigned ndid = alloc_did();
-      if (EXPECT_FALSE(ndid == ~0U))
-        return ~0UL;
-
-      unsigned long none = 0;
-      if (!cxx::atomic_compare_exchange_strong(&_did, none, (unsigned long)ndid))
-        free_did(ndid);
-    }
-  return _did;
-}
-
-IMPLEMENT
 void
 Dmar_space::create_identity_map()
 {
@@ -328,29 +88,6 @@ Dmar_space::create_identity_map()
     }
 }
 
-
-PUBLIC inline
-int
-Dmar_space::resume_vcpu(Context *, Vcpu_state *, bool) override
-{
-  return -L4_err::EInval;
-}
-
-
-IMPLEMENT inline
-unsigned char
-Dmar_space::Dmar_ptr::page_order() const
-{ return Dmar_space::Dmar_pt::page_order_for_level(level); }
-
-IMPLEMENT inline
-Unsigned64
-Dmar_space::Dmar_ptr::page_addr() const
-{
-  unsigned char o = page_order();
-  return cxx::mask_lsb(e->v, o);
-}
-
-IMPLEMENT
 void
 Dmar_space::tlb_flush(bool)
 {
@@ -359,11 +96,10 @@ Dmar_space::tlb_flush(bool)
       Intel::Io_mmu::Inv_desc::iotlb_did(_did));
 }
 
-PUBLIC
 bool
 Dmar_space::v_lookup(Mem_space::Vaddr virt, Mem_space::Phys_addr *phys,
                      Mem_space::Page_order *order,
-                     Mem_space::Attr *page_attribs) override
+                     Mem_space::Attr *page_attribs)
 {
   auto i = _dmarpt->walk(virt);
   // XXX CEH: Check if this hack is still needed!
@@ -382,11 +118,10 @@ Dmar_space::v_lookup(Mem_space::Vaddr virt, Mem_space::Phys_addr *phys,
   return true;
 }
 
-PUBLIC
 Mem_space::Status
 Dmar_space::v_insert(Mem_space::Phys_addr phys, Mem_space::Vaddr virt,
                      Mem_space::Page_order order,
-                     Mem_space::Attr page_attribs) override
+                     Mem_space::Attr page_attribs)
 {
   assert (cxx::is_zero(cxx::get_lsb(Mem_space::Phys_addr(phys), order)));
   assert (cxx::is_zero(cxx::get_lsb(Virt_addr(virt), order)));
@@ -420,10 +155,9 @@ Dmar_space::v_insert(Mem_space::Phys_addr phys, Mem_space::Vaddr virt,
     }
 }
 
-PUBLIC
 L4_fpage::Rights
 Dmar_space::v_delete(Mem_space::Vaddr virt, Mem_space::Page_order order,
-                     L4_fpage::Rights page_attribs) override
+                     L4_fpage::Rights page_attribs)
 {
   assert(cxx::is_zero(cxx::get_lsb(Virt_addr(virt), order)));
 
@@ -445,19 +179,16 @@ Dmar_space::v_delete(Mem_space::Vaddr virt, Mem_space::Page_order order,
   return ret;
 }
 
-PUBLIC
 void
-Dmar_space::v_set_access_flags(Mem_space::Vaddr, L4_fpage::Rights) override
+Dmar_space::v_set_access_flags(Mem_space::Vaddr, L4_fpage::Rights)
 {}
 
 static Mem_space::Fit_size __dmar_ps;
 
-PUBLIC
 Mem_space::Fit_size const &
-Dmar_space::mem_space_fitting_sizes() const override
+Dmar_space::mem_space_fitting_sizes() const
 { return __dmar_ps; }
 
-PRIVATE static
 void
 Dmar_space::add_page_size(Mem_space::Page_order o)
 {
@@ -465,30 +196,21 @@ Dmar_space::add_page_size(Mem_space::Page_order o)
   __dmar_ps.add_page_size(o);
 }
 
-PUBLIC
 void *
-Dmar_space::operator new (size_t size, void *p) throw()
+Dmar_space::operator new (size_t size, void *p) noexcept
 {
   (void)size;
   assert (size == sizeof (Dmar_space));
   return p;
 }
 
-PUBLIC
 void
-Dmar_space::operator delete (void *ptr)
+Dmar_space::operator delete (void *ptr) noexcept
 {
   Dmar_space *t = reinterpret_cast<Dmar_space *>(ptr);
   Kmem_slab_t<Dmar_space>::q_free(t->ram_quota(), ptr);
 }
 
-PUBLIC inline
-Dmar_space::Dmar_space(Ram_quota *q)
-: Dyn_castable_class(q, Caps::mem()),
-  _dmarpt(0), _did(0)
-{}
-
-PRIVATE
 void
 Dmar_space::remove_from_all_iommus()
 {
@@ -541,15 +263,13 @@ Dmar_space::remove_from_all_iommus()
   Intel::Io_mmu::queue_and_wait_on_iommus(need_wait);
 }
 
-PUBLIC
 void
-Dmar_space::destroy(Kobject ***rl) override
+Dmar_space::destroy(Kobject ***rl)
 {
   Task::destroy(rl);
   remove_from_all_iommus();
 }
 
-PUBLIC
 Dmar_space::~Dmar_space()
 {
   remove_from_all_iommus();
---
+++ src/kern/ia32/dmar_space.h
@@ -0,0 +1,280 @@
+#pragma once
+
+#include <task.h>
+#include <ptab_base.h>
+#include <bitmap.h>
+
+class Dmar_space :
+  public cxx::Dyn_castable<Dmar_space, Task>
+{
+private:
+  template<typename T>
+  static void clean_dcache(T *p)
+  { Mem_unit::clean_dcache(p, p + 1); }
+
+  class Dmar_ptr
+  {
+  public:
+    struct Dmar_ptr_val
+    {
+      Unsigned64 v;
+      Dmar_ptr_val() = default;
+      Dmar_ptr_val(Unsigned64 v) : v(v) {}
+      CXX_BITFIELD_MEMBER(0, 1, present, v);
+    };
+
+    Dmar_ptr_val *e;
+
+  public:
+    typedef Mem_space::Attr Attr;
+
+    unsigned char level;
+    Dmar_ptr() = default;
+    Dmar_ptr(Unsigned64 *e, unsigned char l)
+    : e(reinterpret_cast<Dmar_ptr_val*>(e)), level(l) {}
+
+    bool is_valid() const { return e->present(); }
+    bool is_leaf() const
+    { return (level == Dmar_pt::Depth) || (e->v & (1 << 7)); }
+    Unsigned64 next_level() const
+    { return cxx::mask_lsb(e->v, Config::PAGE_SHIFT); }
+
+    void set(Unsigned64 v)
+    {
+      write_consistent(e, Dmar_ptr_val(v));
+      clean_dcache(e);
+    }
+
+    void clear() { set(0); }
+
+    unsigned char page_order() const
+    { return Dmar_space::Dmar_pt::page_order_for_level(level); }
+
+    Unsigned64 page_addr() const
+    {
+      unsigned char o = page_order();
+      return cxx::mask_lsb(e->v, o);
+    }
+
+    Attr attribs() const
+    {
+      typedef L4_fpage::Rights R;
+
+      auto raw = access_once(&e->v);
+
+      R r = R::UR();
+      if (raw & 2) r |= R::W();
+
+      return Attr(r, Page::Type::Normal());
+    }
+
+    bool add_attribs(Page::Attr attr)
+    {
+      typedef L4_fpage::Rights R;
+
+      if (attr.rights & R::W())
+        {
+          auto p = access_once(&e->v);
+          auto o = p;
+          p |= 2;
+          if (o != p)
+            {
+              write_now(&e->v, p);
+              clean_dcache(e);
+              return true;
+            }
+        }
+      return false;
+    }
+
+    void set_next_level(Unsigned64 phys)
+    { set(phys | 3); }
+
+    void write_back_if(bool) const {}
+    static void write_back(void *, void *) {}
+
+    L4_fpage::Rights access_flags() const
+    {
+      return L4_fpage::Rights(0);
+    }
+
+    void del_rights(L4_fpage::Rights r)
+    {
+      if (r & L4_fpage::Rights::W())
+        {
+          auto p = access_once(&e->v);
+          auto o = p & ~(Unsigned64)2;
+          if (o != p)
+            {
+              write_now(&e->v, p);
+              clean_dcache(e);
+            }
+        }
+    }
+
+    /*
+     * WARNING: The VT-d documentation says that the super page bit
+     * WARNING: is ignored in page table entries for 4k pages. However,
+     * WARNING: this is not true. The super page bit must be zero.
+     */
+    void create_page(Phys_mem_addr addr, Page::Attr attr)
+    {
+      typedef L4_fpage::Rights R;
+
+      assert(level <= Dmar_pt::Depth);
+      Unsigned64 r = (level == Dmar_pt::Depth) ? 0 : (Unsigned64)(1<<7);
+      r |= 1; // Read
+      if (attr.rights & R::W()) r |= 2;
+
+      set(cxx::int_value<Phys_mem_addr>(addr) | r);
+    }
+  };
+
+  typedef Ptab::Tupel<Ptab::Traits<Unsigned64, 39, 9, true>,
+                      Ptab::Traits<Unsigned64, 30, 9, true>,
+                      Ptab::Traits<Unsigned64, 21, 9, true>,
+                      Ptab::Traits<Unsigned64, 12, 9, true> >::List Dmar_traits;
+
+  typedef Ptab::Shift<Dmar_traits, 12>::List Dmar_traits_vpn;
+  typedef Ptab::Page_addr_wrap<Page_number, 12> Dmar_va_vpn;
+  typedef Ptab::Base<Dmar_ptr, Dmar_traits_vpn, Dmar_va_vpn, Mem_layout> Dmar_pt;
+
+public:
+  enum { Max_nr_did = 0x10000 };
+
+  explicit Dmar_space(Ram_quota *q)
+  : Dyn_castable_class(q, Caps::mem())
+  {}
+
+  ~Dmar_space() noexcept;
+
+  virtual void *debug_dir() const { return (void *)_dmarpt; }
+
+  Mword get_root(int aw_level) const
+  {
+    return get_root(_dmarpt, aw_level);
+  }
+
+  unsigned long get_did()
+  {
+    // XXX: possibly need a loop here
+    if (_did == 0)
+      {
+        unsigned ndid = alloc_did();
+        if (EXPECT_FALSE(ndid == ~0U))
+          return ~0UL;
+
+        unsigned long none = 0;
+        if (!cxx::atomic_compare_exchange_strong(&_did, none, (unsigned long)ndid))
+          free_did(ndid);
+      }
+    return _did;
+  }
+
+  bool initialize()
+  {
+    void *b;
+
+    if (!_initialized)
+      return false;
+
+    b = Kmem_alloc::allocator()->q_alloc(ram_quota(), Config::page_order());
+    if (EXPECT_FALSE(!b))
+      return false;
+
+    _dmarpt = static_cast<Dmar_pt *>(b);
+    _dmarpt->clear(false);
+
+    /*
+     * Make sure that the very first entry in a page table is valid and
+     * not a super page. This is necessary if the hardware supports
+     * fewer levels than the current software implementation.
+     *
+     * Force allocation of two levels in entry 0, so get_root works
+     */
+    auto i = _dmarpt->walk(Mem_space::V_pfn(0), 2, false,
+                           Kmem_alloc::q_allocator(ram_quota()));
+    if (i.level != 2)
+      {
+        // Got a page-table entry with the wrong level. That happens in the
+        // case of an out-of-memory situation. So free everything we already
+        // allocated and fail.
+        _dmarpt->destroy(Virt_addr(0UL), Virt_addr(~0UL), 0, Dmar_pt::Depth,
+                         Kmem_alloc::q_allocator(ram_quota()));
+        Kmem_alloc::allocator()->q_free(ram_quota(), Config::page_order(), _dmarpt);
+        _dmarpt = 0;
+        return false;
+      }
+
+    return true;
+  }
+
+  void tlb_flush(bool) override;
+  Page_number mem_space_map_max_address() const override;
+  int resume_vcpu(Context *, Vcpu_state *, bool) override
+  {
+    return -L4_err::EInval;
+  }
+
+  bool
+  v_lookup(Mem_space::Vaddr virt, Mem_space::Phys_addr *phys,
+           Mem_space::Page_order *order,
+           Mem_space::Attr *page_attribs) override;
+
+  Mem_space::Status
+  v_insert(Mem_space::Phys_addr phys, Mem_space::Vaddr virt,
+           Mem_space::Page_order order,
+           Mem_space::Attr page_attribs) override;
+
+  L4_fpage::Rights
+  v_delete(Mem_space::Vaddr virt, Mem_space::Page_order order,
+           L4_fpage::Rights page_attribs) override;
+
+  void
+  v_set_access_flags(Mem_space::Vaddr, L4_fpage::Rights) override;
+
+  Mem_space::Fit_size const &
+  mem_space_fitting_sizes() const override;
+
+  static void add_page_size(Mem_space::Page_order o);
+
+  void *operator new (size_t size, void *p) noexcept;
+  void operator delete (void *ptr) noexcept;
+  void destroy(Kobject ***rl) override;
+
+  static Mword get_root(Dmar_pt *pt, unsigned aw_level)
+  {
+    aw_level += 2;
+    if (aw_level == Dmar_pt::Depth + 1)
+      return Mem_layout::pmem_to_phys(pt);
+
+    assert(aw_level <= Dmar_pt::Depth);
+
+    auto i = pt->walk(Mem_space::V_pfn(0), Dmar_pt::Depth - aw_level);
+    assert(i.is_valid());
+    return i.next_level();
+  }
+
+  static void init(unsigned max_did);
+
+  static void create_identity_map();
+
+  static Dmar_pt *identity_map;
+
+private:
+  Dmar_pt *_dmarpt = nullptr;
+  unsigned long _did = 0;
+
+  static bool _initialized;
+
+  typedef Bitmap<Max_nr_did> Did_map;
+
+  static Did_map *_free_dids;
+  static unsigned _max_did;
+
+  static unsigned alloc_did();
+  static void free_did(unsigned long did);
+
+  void remove_from_all_iommus();
+};
+
