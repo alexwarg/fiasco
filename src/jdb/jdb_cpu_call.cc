90ac035e66d75778a649e2cf9e13be73ac597067
Alexander Warg <alex@wargs.de>
2026-05-23 12:22:15 +0200

    jdb_cpu_calls.cpp conversion to plain C++
    Change-Id: I876b0de315237d6e3ba7213310e1ab57cd96f911

---
+++ src/jdb/jdb_cpu_call.cc
@@ -0,0 +1,56 @@
+
+#include <cstdio>
+#include "simpleio.h"
+
+#include "cpu_call.h"
+#include "jdb.h"
+#include "jdb_module.h"
+#include "static_init.h"
+#include "types.h"
+
+class Jdb_cpu_call_module : public Jdb_module
+{
+public:
+  Jdb_cpu_call_module() FIASCO_INIT;
+
+  Action_code action(int cmd, void *&, char const *&, int &) override
+  {
+    if (cmd)
+      return NOTHING;
+
+    Jdb::foreach_cpu(&print_info);
+
+    return NOTHING;
+  }
+
+  int num_cmds() const override
+  { return 1; }
+
+  Cmd const *cmds() const override
+  {
+    static Cmd cs[] =
+      { { 0, "", "cpucalls", "", "cpucalls\tshow CPU calls", 0 } };
+
+    return cs;
+  }
+
+private:
+  static void print_info(Cpu_number cpu)
+  {
+    Cpu_call_queue &q = Cpu_call::_glbl_q.cpu(cpu);
+    Queue_item *qi = q.first();
+    if (qi)
+      {
+        Cpu_call *r = static_cast<Cpu_call*>(qi);
+        printf("CPU%02u wait: %ld, queued: %d\n",
+               cxx::int_value<Cpu_number>(cpu), r->_wait, r->queued());
+      }
+  }
+
+};
+
+static Jdb_cpu_call_module jdb_cpu_call_module INIT_PRIORITY(JDB_MODULE_INIT_PRIO);
+
+Jdb_cpu_call_module::Jdb_cpu_call_module()
+  : Jdb_module("INFO")
+{}
