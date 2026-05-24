45fd56f314562c6753a496f9d55a8db96cba5659
Alexander Warg <alex@wargs.de>
2026-05-24 18:13:40 +0200

    JDB: jdb_core conversion to plain C++
    Change-Id: I57aa0b7d8865be75c72950147fd9de40a47c8a39

--- src/jdb/jdb_core.cc
+++ src/jdb/jdb_core.cc
@@ -1,139 +1,4 @@
-INTERFACE:
-
-#include "static_init.h"
-#include "jdb_module.h"
-
-/**
- * The core of the modularized Jdb.
- * @see Jdb_module
- * @see Jdb_category
- *
- * This class provides the core functions for handling
- * Jdb_modules and providing them with the right input.
- *
- */
-class Jdb_core
-{
-public:
-
-  /**
-   * The command structure for Jdb_core.
-   *
-   * This structure consists of a pointer to the Jdb_module
-   * and a Jdb_module::Cmd structure. It is used in exec_cmd()
-   * and returned from has_cmd().
-   */
-  struct Cmd
-  {
-    /**
-     * Pointer to the module providing this command.
-     */
-    Jdb_module            *mod;
-
-    /**
-     * The Jdb_module::Cmd structure, describing the command.
-     *
-     * If this is a null pointer the command is invalid.
-     * @see Jdb_module
-     * @see Jdb_module::Cmd
-     */
-    Jdb_module::Cmd const *cmd;
-
-    /**
-     * Create a Jdb_core::Cmd.
-     * @param _mod the Jdb_module providing the command.
-     * @param _cmd the command structure (see Jdb_module::Cmd).
-     */
-    Cmd(Jdb_module *_mod, Jdb_module::Cmd const *_cmd = 0)
-      : mod(_mod), cmd(_cmd)
-    {}
-  };
-
-  /**
-   * Get the command structure accoring to the given name.
-   * @param cmd the command to look for.
-   * @return A valid Cmd structure if cmd was found, or a
-   *         Cmd structure where Cmd::cmd is a null pointer if
-   *         no module provides such a command.
-   */
-  static Cmd has_cmd(char const *cmd);
-
-  /**
-   * Execute the command according to cmd.
-   * @param cmd the command structure (see Jdb_core::Cmd), which
-   *        describes the command to execute.
-   * @return 0 if Jdb_module::action() returned LEAVE
-   *         1 if Jdb_module::action() returned NOTHING
-   *         2 if Jdb_module::action() returned GO_BACK (KEY_HOME entered)
-   *         3 if the input was aborted (KEY_ESC entered) or was invalid
-   *
-   * This method is actually responsible for reading the input
-   * with respect to the commands format string and calling
-   * the Jdb_module::action() method after that.
-   *
-   */
-  static int exec_cmd(Cmd const cmd, char const *str, int push_next_char = -1);
-
-  /**
-   * Overwritten getchar() to be able to handle next_char.
-   */
-  static int getchar(void);
-
-  /**
-   * Call this function every time a `\n' is written to the
-   *        console and it stops output when the screen is full.
-   * @return 0 if user wants to abort the output (escape or 'q' pressed)
-   */
-  static int new_line(unsigned &line);
-
-  static void prompt_start();
-  static void prompt_end();
-  static void prompt();
-  static int  prompt_len();
-  static int set_prompt_color(char v);
-
-  /**
-   * Like strlen but do not count ESC sequences.
-   */
-  static int print_len(const char *s);
-
-  static char esc_prompt[];
-
-  static void (*wait_for_input)();
-
-public:
-  static bool short_mode;
-  typedef int (Input_fmt)(char fmt, int *size, char const *cmd_str, void *buf);
-
-private:
-  static int  next_char;
-  static Input_fmt *_fmt_list[26];
-};
-
-#define JDB_ANSI_black        "30"
-#define JDB_ANSI_gray         "30;1"
-#define JDB_ANSI_red          "31"
-#define JDB_ANSI_lightred     "31;1"
-#define JDB_ANSI_green        "32"
-#define JDB_ANSI_lightgreen   "32;1"
-#define JDB_ANSI_brown        "33"
-#define JDB_ANSI_yellow       "33;1"
-#define JDB_ANSI_blue         "34"
-#define JDB_ANSI_lightblue    "34;1"
-#define JDB_ANSI_magenta      "35"
-#define JDB_ANSI_lightmagenta "35;1"
-#define JDB_ANSI_cyan         "36"
-#define JDB_ANSI_lightcyan    "36;1"
-#define JDB_ANSI_white        "37"
-#define JDB_ANSI_brightwhite  "37;1"
-#define JDB_ANSI_default      ""
-
-#define JDB_ANSI_COLOR(color)   "\033[" JDB_ANSI_##color "m"
-
-#define JDB_ANSI_END          "\033[m"
-
-
-IMPLEMENTATION:
+#include "jdb_core.h"
 
 #include <cstring>
 #include <cstdarg>
@@ -156,7 +21,6 @@ char Jdb_core::esc_prompt[32] = JDB_ANSI_COLOR(green);
 Jdb_core::Input_fmt *Jdb_core::_fmt_list[26]; 
 void (*Jdb_core::wait_for_input)();
 
-PUBLIC static
 bool
 Jdb_core::add_fmt_handler(char fmt, Input_fmt* hdlr)
 {
@@ -170,24 +34,16 @@ Jdb_core::add_fmt_handler(char fmt, Input_fmt* hdlr)
   return true;
 }
 
-PUBLIC static
-void
-Jdb_core::print_prompt()
-{ prompt_start(); prompt(); prompt_end(); }
-
-IMPLEMENT
 void Jdb_core::prompt_start()
 {
   putstr(esc_prompt);
 }
 
-IMPLEMENT
 void Jdb_core::prompt_end()
 {
   putstr(JDB_ANSI_END);
 }
 
-IMPLEMENT
 void Jdb_core::prompt()
 {
   Jdb_prompt_ext::do_all();
@@ -197,13 +53,11 @@ void Jdb_core::prompt()
     putstr("jdb# ");
 }
 
-IMPLEMENT
 int Jdb_core::prompt_len()
 {
   return 5;
 }
 
-IMPLEMENT
 int Jdb_core::print_len(const char *s)
 {
   int l = 0;
@@ -226,26 +80,6 @@ int Jdb_core::print_len(const char *s)
   return l;
 }
 
-PUBLIC static
-int
-Jdb_core::get_ansi_color(char c)
-{
-  switch(c)
-    {
-    case 'N': case 'n': return 30;
-    case 'R': case 'r': return 31;
-    case 'G': case 'g': return 32;
-    case 'Y': case 'y': return 33;
-    case 'B': case 'b': return 34;
-    case 'M': case 'm': return 35;
-    case 'C': case 'c': return 36;
-    case 'W': case 'w': return 37;
-    default:  return 0;
-    }
-}
-
-
-IMPLEMENT
 int Jdb_core::set_prompt_color(char x)
 {
   unsigned pc = get_ansi_color(x);
@@ -262,7 +96,6 @@ int Jdb_core::set_prompt_color(char x)
 
 }
 
-IMPLEMENT
 Jdb_core::Cmd Jdb_core::has_cmd(char const *cmd)
 {
   for (auto const &&m: Jdb_module::modules)
@@ -276,7 +109,6 @@ Jdb_core::Cmd Jdb_core::has_cmd(char const *cmd)
   return Cmd(0);
 }
 
-PRIVATE static
 unsigned
 Jdb_core::match_len(char const *a, char const *b, unsigned l)
 {
@@ -288,7 +120,6 @@ Jdb_core::match_len(char const *a, char const *b, unsigned l)
   return p;
 }
 
-PUBLIC static
 unsigned
 Jdb_core::print_alternatives(char const *prefix)
 {
@@ -317,7 +148,6 @@ Jdb_core::print_alternatives(char const *prefix)
   return prefix_len;
 }
 
-PUBLIC static
 Jdb_core::Cmd
 Jdb_core::complete_cmd(char const *prefix, bool &multi_match)
 {
@@ -343,7 +173,6 @@ Jdb_core::complete_cmd(char const *prefix, bool &multi_match)
   return match;
 }
 
-IMPLEMENT
 int Jdb_core::getchar()
 {
   if (next_char != -1)
@@ -368,7 +197,6 @@ int Jdb_core::getchar()
     }
 }
 
-PUBLIC static
 int
 Jdb_core::cmd_getchar(char const *&str)
 {
@@ -391,13 +219,7 @@ Jdb_core::cmd_getchar(char const *&str)
   return *(str++);
 }
 
-PUBLIC static inline NEEDS[<cstdio>]
-void
-Jdb_core::cmd_putchar(int c)
-{ if (short_mode) putchar(c); }
-
-IMPLEMENT
-int Jdb_core::exec_cmd(Cmd const cmd, char const *str, int push_next_char = -1)
+int Jdb_core::exec_cmd(Cmd const cmd, char const *str, int push_next_char)
 {
   char const* f = cmd.cmd->fmt;
   char const* f1;
@@ -756,7 +578,6 @@ int Jdb_core::exec_cmd(Cmd const cmd, char const *str, int push_next_char = -1)
 
 }
 
-IMPLEMENT
 int
 Jdb_core::new_line( unsigned &line )
 {
@@ -801,40 +622,37 @@ class Go_m : public Jdb_module
 {
 public:
   Go_m() FIASCO_INIT;
+
+  Action_code action( int, void *&, char const *&, int & ) override
+  {
+    putchar('\n');
+    return LEAVE;
+  }
+
+  int num_cmds() const override
+  {
+    return 1;
+  }
+
+  Cmd const *cmds() const override
+  {
+    static Cmd cs[] =
+      { 
+          { 0, "g", "go", "",
+             "g\tleave kernel debugger\n"
+             "Return\tshow debug message", 0 },
+      };
+
+    return cs;
+  }
+
 };
 
 static Go_m go_m INIT_PRIORITY(JDB_MODULE_INIT_PRIO);
 
-IMPLEMENT
 Go_m::Go_m()
   : Jdb_module("GENERAL")
 {}
-
-PUBLIC
-Jdb_module::Action_code Go_m::action( int, void *&, char const *&, int & ) override
-{
-  putchar('\n');
-  return LEAVE;
-}
-
-PUBLIC
-int Go_m::num_cmds() const override
-{ 
-  return 1;
-}
-
-PUBLIC
-Jdb_module::Cmd const * Go_m::cmds() const override
-{
-  static Cmd cs[] =
-    { 
-	{ 0, "g", "go", "",
-	   "g\tleave kernel debugger\n"
-	   "Return\tshow debug message", 0 },
-    };
-
-  return cs;
-}
 
 
 /**
@@ -847,119 +665,114 @@ class Help_m : public Jdb_module
 {
 public:
   Help_m() FIASCO_INIT;
+
+  Action_code action(int, void *&, char const *&, int &) override
+  {
+    size_t const tab_width = 27;
+
+    if(Jdb_category::categories.begin() == Jdb_category::categories.end())
+      {
+        printf("No debugger commands seem to have been registered\n");
+        return NOTHING;
+      }
+
+    unsigned line = 0;
+
+    puts("");
+    for (auto const &&c: Jdb_category::categories)
+      {
+        bool first = true;
+        for (auto const &&m: Jdb_module::modules)
+          {
+            if (m->category() != c)
+              continue;
+
+            if(first)
+              {
+                if(!Jdb_core::new_line(line))
+                  return NOTHING;
+                printf("\033[1m[%s]\033[0m %s", c->name(), c->description());
+                putchar('\n');
+                if(!Jdb_core::new_line(line))
+                  return NOTHING;
+                first = false;
+              }
+            unsigned ncmds = m->num_cmds();
+            Jdb_module::Cmd const *cmds = m->cmds();
+            for(unsigned x = 0; x < ncmds; x++)
+              {
+                char const *descr = cmds[x].descr;
+                if (!descr)
+                  continue;
+                size_t pos = strcspn(descr, "\n\t");
+                size_t xpos = 2;
+                putstr("  ");
+                while( pos  < strlen(descr))
+                  {
+                    putnstr(descr, pos);
+                    xpos += pos;
+                    switch(descr[pos])
+                      {
+                      case '\n':
+                        putchar('\n');
+                        if(!Jdb_core::new_line(line))
+                          return NOTHING;
+                        putstr("  ");
+                        xpos = 2;
+                        break;
+                      case '\t':
+                        if(xpos<tab_width)
+                          {
+                            putnstr("                            ",
+                                    tab_width-xpos);
+                            xpos = tab_width;
+                          }
+                        else
+                          {
+                            putchar(' ');
+                            xpos++;
+                          }
+                        break;
+                      default:
+                        break;
+                      }
+                    descr = descr + pos + 1;
+                    pos = strcspn(descr, "\n\t");
+                  }
+
+                putstr(descr);
+                putchar('\n');
+                if(!Jdb_core::new_line(line))
+                  return NOTHING;
+              }
+          }
+      }
+
+    putchar('\n');
+
+    return NOTHING;
+  }
+
+  int num_cmds() const override
+  {
+    return 2;
+  }
+
+  Cmd const *cmds() const override
+  {
+    static Cmd cs[] =
+      {
+          { 0, "h", "help", "", "h\tShow this help screen.", 0 },
+          { 0, "?", 0, "", 0, 0 },
+      };
+
+    return cs;
+  }
 };
 
 
 static Help_m help_m INIT_PRIORITY(JDB_MODULE_INIT_PRIO);
 
-
-PUBLIC
-Jdb_module::Action_code Help_m::action(int, void *&, char const *&, int &) override
-{
-  size_t const tab_width = 27;
-
-  if(Jdb_category::categories.begin() == Jdb_category::categories.end())
-    {
-      printf("No debugger commands seem to have been registered\n");
-      return NOTHING;
-    }
-
-  unsigned line = 0;
-
-  puts("");
-  for (auto const &&c: Jdb_category::categories)
-    {
-      bool first = true;
-      for (auto const &&m: Jdb_module::modules)
-	{
-          if (m->category() != c)
-            continue;
-
-	  if(first)
-	    {
-	      if(!Jdb_core::new_line(line))
-		return NOTHING;
-	      printf("\033[1m[%s]\033[0m %s", c->name(), c->description());
-	      putchar('\n');
-	      if(!Jdb_core::new_line(line))
-		return NOTHING;
-	      first = false;
-	    }
-	  unsigned ncmds = m->num_cmds();
-	  Jdb_module::Cmd const *cmds = m->cmds();
-	  for(unsigned x = 0; x < ncmds; x++)
-	    {
-	      char const *descr = cmds[x].descr;
-	      if (!descr)
-		continue;
-	      size_t pos = strcspn(descr, "\n\t");
-	      size_t xpos = 2;
-	      putstr("  ");
-	      while( pos  < strlen(descr))
-		{
-		  putnstr(descr, pos);
-		  xpos += pos;
-		  switch(descr[pos])
-		    {
-		    case '\n':
-		      putchar('\n');
-		      if(!Jdb_core::new_line(line))
-			return NOTHING;
-		      putstr("  ");
-		      xpos = 2;
-		      break;
-		    case '\t':
-		      if(xpos<tab_width)
-			{
-			  putnstr("                            ",
-				  tab_width-xpos);
-			  xpos = tab_width;
-			}
-		      else
-			{
-			  putchar(' ');
-			  xpos++;
-			}
-		      break;
-		    default:
-		      break;
-		    }
-		  descr = descr + pos + 1;
-		  pos = strcspn(descr, "\n\t");
-		}
-
-	      putstr(descr);
-	      putchar('\n');
-	      if(!Jdb_core::new_line(line))
-		return NOTHING;
-	    }
-	}
-    }
-
-  putchar('\n');
-
-  return NOTHING;
-}
-
-PUBLIC
-int Help_m::num_cmds() const override
-{ 
-  return 2;
-}
-
-PUBLIC
-Jdb_module::Cmd const * Help_m::cmds() const override
-{
-  static Cmd cs[] =
-    {
-	{ 0, "h", "help", "", "h\tShow this help screen.", 0 },
-	{ 0, "?", 0, "", 0, 0 },
-    };
-
-  return cs;
-}
-
-IMPLEMENT
 Help_m::Help_m()
   : Jdb_module("GENERAL")
 {}
---
+++ src/jdb/jdb_core.h
@@ -0,0 +1,164 @@
+#pragma once
+
+#include "static_init.h"
+#include "jdb_module.h"
+#include <stdio.h>
+
+/**
+ * The core of the modularized Jdb.
+ * @see Jdb_module
+ * @see Jdb_category
+ *
+ * This class provides the core functions for handling
+ * Jdb_modules and providing them with the right input.
+ *
+ */
+class Jdb_core
+{
+public:
+  typedef int (Input_fmt)(char fmt, int *size, char const *cmd_str, void *buf);
+
+  /**
+   * The command structure for Jdb_core.
+   *
+   * This structure consists of a pointer to the Jdb_module
+   * and a Jdb_module::Cmd structure. It is used in exec_cmd()
+   * and returned from has_cmd().
+   */
+  struct Cmd
+  {
+    /**
+     * Pointer to the module providing this command.
+     */
+    Jdb_module            *mod;
+
+    /**
+     * The Jdb_module::Cmd structure, describing the command.
+     *
+     * If this is a null pointer the command is invalid.
+     * @see Jdb_module
+     * @see Jdb_module::Cmd
+     */
+    Jdb_module::Cmd const *cmd;
+
+    /**
+     * Create a Jdb_core::Cmd.
+     * @param _mod the Jdb_module providing the command.
+     * @param _cmd the command structure (see Jdb_module::Cmd).
+     */
+    Cmd(Jdb_module *_mod, Jdb_module::Cmd const *_cmd = 0)
+      : mod(_mod), cmd(_cmd)
+    {}
+  };
+
+  /**
+   * Get the command structure accoring to the given name.
+   * @param cmd the command to look for.
+   * @return A valid Cmd structure if cmd was found, or a
+   *         Cmd structure where Cmd::cmd is a null pointer if
+   *         no module provides such a command.
+   */
+  static Cmd has_cmd(char const *cmd);
+
+  /**
+   * Execute the command according to cmd.
+   * @param cmd the command structure (see Jdb_core::Cmd), which
+   *        describes the command to execute.
+   * @return 0 if Jdb_module::action() returned LEAVE
+   *         1 if Jdb_module::action() returned NOTHING
+   *         2 if Jdb_module::action() returned GO_BACK (KEY_HOME entered)
+   *         3 if the input was aborted (KEY_ESC entered) or was invalid
+   *
+   * This method is actually responsible for reading the input
+   * with respect to the commands format string and calling
+   * the Jdb_module::action() method after that.
+   *
+   */
+  static int exec_cmd(Cmd const cmd, char const *str, int push_next_char = -1);
+
+  /**
+   * Overwritten getchar() to be able to handle next_char.
+   */
+  static int getchar(void);
+
+  /**
+   * Call this function every time a `\n' is written to the
+   *        console and it stops output when the screen is full.
+   * @return 0 if user wants to abort the output (escape or 'q' pressed)
+   */
+  static int new_line(unsigned &line);
+
+  static void prompt_start();
+  static void prompt_end();
+  static void prompt();
+  static int  prompt_len();
+  static int set_prompt_color(char v);
+
+  /**
+   * Like strlen but do not count ESC sequences.
+   */
+  static int print_len(const char *s);
+
+  static char esc_prompt[];
+
+  static void (*wait_for_input)();
+
+  static bool add_fmt_handler(char fmt, Input_fmt* hdlr);
+  static unsigned print_alternatives(char const *prefix);
+  static Cmd complete_cmd(char const *prefix, bool &multi_match);
+  static int cmd_getchar(char const *&str);
+
+  static void print_prompt() { prompt_start(); prompt(); prompt_end(); }
+
+  static void cmd_putchar(int c)
+  { if (short_mode) putchar(c); }
+
+  static int get_ansi_color(char c)
+  {
+    switch(c)
+      {
+      case 'N': case 'n': return 30;
+      case 'R': case 'r': return 31;
+      case 'G': case 'g': return 32;
+      case 'Y': case 'y': return 33;
+      case 'B': case 'b': return 34;
+      case 'M': case 'm': return 35;
+      case 'C': case 'c': return 36;
+      case 'W': case 'w': return 37;
+      default:  return 0;
+      }
+  }
+
+
+public:
+  static bool short_mode;
+
+private:
+  static unsigned match_len(char const *a, char const *b, unsigned l);
+
+  static int  next_char;
+  static Input_fmt *_fmt_list[26];
+};
+
+#define JDB_ANSI_black        "30"
+#define JDB_ANSI_gray         "30;1"
+#define JDB_ANSI_red          "31"
+#define JDB_ANSI_lightred     "31;1"
+#define JDB_ANSI_green        "32"
+#define JDB_ANSI_lightgreen   "32;1"
+#define JDB_ANSI_brown        "33"
+#define JDB_ANSI_yellow       "33;1"
+#define JDB_ANSI_blue         "34"
+#define JDB_ANSI_lightblue    "34;1"
+#define JDB_ANSI_magenta      "35"
+#define JDB_ANSI_lightmagenta "35;1"
+#define JDB_ANSI_cyan         "36"
+#define JDB_ANSI_lightcyan    "36;1"
+#define JDB_ANSI_white        "37"
+#define JDB_ANSI_brightwhite  "37;1"
+#define JDB_ANSI_default      ""
+
+#define JDB_ANSI_COLOR(color)   "\033[" JDB_ANSI_##color "m"
+
+#define JDB_ANSI_END          "\033[m"
+
