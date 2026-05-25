6f99fafcc67ed58652fd54d2ef1fe554dbf8d0e4
Alexander Warg <alex@wargs.de>
2026-05-25 22:27:49 +0200

    jdb_factory to C++ conversion
    Change-Id: I4803254852bee0908f84390ac0a1d52af74dcc82

--- src/jdb/jdb_factory.cc
+++ src/jdb/jdb_factory.cc
@@ -1,4 +1,3 @@
-IMPLEMENTATION:
 
 #include <climits>
 #include <cstring>
@@ -19,28 +18,24 @@ class Jdb_factory : public Jdb_kobject_handler
 {
 public:
   Jdb_factory() FIASCO_INIT;
+
+  bool show_kobject(Kobject_common *, int) override
+  {
+    return true;
+  }
+
+  void show_kobject_short(String_buffer *buf, Kobject_common *o, bool) override
+  {
+    Factory *t = cxx::dyn_cast<Factory*>(o);
+    buf->printf(" c=%lu l=%lu", t->current(), t->limit());
+  }
 };
 
-IMPLEMENT
 Jdb_factory::Jdb_factory()
   : Jdb_kobject_handler((Factory*)0)
 {
   Jdb_kobject::module()->register_handler(this);
 }
 
-PUBLIC
-bool
-Jdb_factory::show_kobject(Kobject_common *, int ) override
-{
-  return true;
-}
-
-PUBLIC
-void
-Jdb_factory::show_kobject_short(String_buffer *buf, Kobject_common *o, bool) override
-{
-  Factory *t = cxx::dyn_cast<Factory*>(o);
-  buf->printf(" c=%lu l=%lu", t->current(), t->limit());
-}
 
 static Jdb_factory jdb_factory INIT_PRIORITY(JDB_MODULE_INIT_PRIO);
