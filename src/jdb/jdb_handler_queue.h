d21fb5d09ae6d3d3aa425386ba64b20a2fc92278
Alexander Warg <alex@wargs.de>
2026-05-25 18:20:27 +0200

    jdb_handler_queue to C++ header conversion
    Change-Id: I5d1ac4da16bdb68dc6ea3b17581f637198d4deda

--- src/jdb/jdb_handler_queue.h
+++ src/jdb/jdb_handler_queue.h
@@ -1,4 +1,5 @@
-INTERFACE:
+
+#pragma once
 
 // XXX If there is a constructor needed for this class make sure that it is
 //     called with a priority higher than JDB_MODULE_INIT_PRIO!
@@ -6,7 +7,7 @@ INTERFACE:
 class Jdb_handler
 {
   friend class Jdb_handler_queue;
-  
+
 public:
   Jdb_handler( void (*handler)() ) : handler(handler), next(0) {}
   void execute() { handler(); }
@@ -18,29 +19,25 @@ private:
 
 class Jdb_handler_queue
 {
+public:
+  void add(Jdb_handler *handler)
+  {
+    handler->next = first;
+    first = handler;
+  }
+
+  void execute() const
+  {
+    Jdb_handler *h = first;
+    while(h)
+      {
+        h->execute();
+        h = h->next;
+      }
+  }
+
 private:
   Jdb_handler *first;
 };
 
-IMPLEMENTATION:
-
-PUBLIC inline
-void
-Jdb_handler_queue::add (Jdb_handler *handler)
-{
-  handler->next = first;
-  first = handler;
-}
-
-PUBLIC
-void
-Jdb_handler_queue::execute () const
-{
-  Jdb_handler *h = first;
-  while(h)
-    {
-      h->execute();
-      h = h->next;
-    }
-}
 
