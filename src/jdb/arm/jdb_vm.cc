8514998207129fddfbe809dbbd2dfd221af9d95f
Alexander Warg <alex@wargs.de>
2026-05-26 19:41:46 +0200

    arm/jdb_vm.cpp to C++ conversion
    Change-Id: Id443c79f570242562c4b7490cda49e38f819c437

--- src/jdb/arm/jdb_vm.cc
+++ src/jdb/arm/jdb_vm.cc
@@ -1,4 +1,3 @@
-IMPLEMENTATION [arm_em_tz]:
 
 #include <cstring>
 #include <cstdio>
@@ -12,41 +11,35 @@ class Jdb_vm : public Jdb_kobject_handler
 {
 public:
   Jdb_vm() FIASCO_INIT;
+
+  bool show_kobject(Kobject_common *o, int lvl)
+  {
+    cxx::dyn_cast<Vm *>(o)->dump_vm_state();
+    if (lvl)
+      {
+        Jdb::getchar();
+        return true;
+      }
+
+    return false;
+  }
+
+  char const *kobject_type(Kobject_common *) const
+  {
+    return JDB_ANSI_COLOR(yellow) "Vm" JDB_ANSI_COLOR(default);
+  }
+
+  void show_kobject_short(String_buffer *buf, Kobject_common *o, bool) override
+  {
+    return cxx::dyn_cast<Vm *>(o)->show_short(buf);
+  }
+
 };
 
-IMPLEMENT
 Jdb_vm::Jdb_vm()
   : Jdb_kobject_handler((Vm *)0)
 {
   Jdb_kobject::module()->register_handler(this);
-}
-
-PUBLIC
-bool
-Jdb_vm::show_kobject(Kobject_common *o, int lvl)
-{
-  cxx::dyn_cast<Vm *>(o)->dump_vm_state();
-  if (lvl)
-    {
-      Jdb::getchar();
-      return true;
-    }
-
-  return false;
-}
-
-PUBLIC
-char const *
-Jdb_vm::kobject_type(Kobject_common *) const
-{
-  return JDB_ANSI_COLOR(yellow) "Vm" JDB_ANSI_COLOR(default);
-}
-
-PUBLIC
-void
-Jdb_vm::show_kobject_short(String_buffer *buf, Kobject_common *o, bool) override
-{
-  return cxx::dyn_cast<Vm *>(o)->show_short(buf);
 }
 
 static Jdb_vm jdb_vm INIT_PRIORITY(JDB_MODULE_INIT_PRIO);
