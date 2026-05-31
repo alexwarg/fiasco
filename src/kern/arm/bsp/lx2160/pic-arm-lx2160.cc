6471336c4c0fdeb1346de0b3893dbe36314427d1
Alexander Warg <alex@wargs.de>
2026-05-31 15:18:30 +0200

    enable MSI support for LX2160
    See ticket #CD-874.
    
    Change-Id: Iff24e5ed978fa69767c071ed5860f6e2e57852dc

--- src/kern/arm/bsp/lx2160/pic-arm-lx2160.cc
+++ src/kern/arm/bsp/lx2160/pic-arm-lx2160.cc
@@ -3,5 +3,7 @@
 
 Pic_gic::Gic_info const Pic_gic::primary_gic_info = Pic_gic::gic_v3_info(
     0x06000000,  // distributor
-    0x06200000, 0x200000); // redist
+    0x06200000, 0x200000, // redist
+    0x06020000, 0x00020000, // its
+);
 
