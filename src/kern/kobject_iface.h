0bedae0084dec751054411eea6b828e09b99bc78
Alexander Warg <alex@wargs.de>
2026-06-18 22:11:06 +0200

    Add generic Kobject::Typed_ref<> as re-evaluable this
    This change add a template type that can be used as re-evaluable
    'this' pointer in Kobject::invoke function implementations and passed
    to the re-evaluating Switch_lock::try_lock, or to Kobject::Locked<>.
    
    It can also be used manually with possibly blocking code.
    
    Change-Id: I6fe684dc1498924c3fc141ed43955f9d34967881

--- src/kern/kobject_iface.h
+++ src/kern/kobject_iface.h
@@ -2,6 +2,7 @@
 
 #include "l4_types.h"
 #include <cxx/dyn_cast>
+#include <cstdint>
 
 class Kobject;
 class Kobject_dbg;
@@ -34,6 +35,65 @@ public:
 class Kobject_iface : public cxx::Dyn_castable<Kobject_iface, Kobject_common>
 {
 public:
+
+  /**
+   * Reevaluable object reference.
+   *
+   * This is a typed reference to a kernel object that facilitates reevaluation
+   * and re-checking of capability references, for example, after preemption points
+   * or potential blocking.
+   *
+   * The reference therefore takes the origin of the object pointer into account.
+   * In case the reference is comming from a special capability (reply cap, or self)
+   * there is no need and no possibility to reevaluate the reference.
+   * However, if the reference comes from a real capability lookup the lookup is
+   * repeated in the eval() function and rights must be the same as before the lookup.
+   */
+  template<typename T, typename OSPC>
+  class Typed_ref
+  {
+  private:
+    uintptr_t _obj_or_space;
+    L4_obj_ref _ref;
+
+    constexpr T *_obj() const { return reinterpret_cast<T *>(_obj_or_space & ~3ul); }
+    constexpr unsigned char _rights() const { return _obj_or_space & 3ul; }
+    constexpr OSPC *_space() const { return reinterpret_cast<OSPC *>(_obj_or_space & ~3ul); }
+
+    static constexpr bool is_obj_ptr(L4_obj_ref ref)
+    { return (ref.op() & L4_obj_ref::Ipc_reply) || (ref.special() && ref.self()); }
+
+  public:
+    constexpr bool is_obj_ptr() const
+    { return is_obj_ptr(_ref); }
+
+    constexpr Typed_ref(T *obj, L4_obj_ref ref, L4_fpage::Rights r, OSPC *space)
+    : _obj_or_space(is_obj_ptr(ref)
+        ? reinterpret_cast<uintptr_t>(obj) | (cxx::int_value<L4_fpage::Rights>(r) & 3ul)
+        : reinterpret_cast<uintptr_t>(space) | (cxx::int_value<L4_fpage::Rights>(r) & 3ul)),
+      _ref(ref)
+    {}
+
+    T *reeval() const
+    {
+      if (is_obj_ptr())
+        return _obj();
+
+      return _space()->lookup_local(_ref.cap(), L4_fpage::Rights(_rights())).template deref<T>();
+    }
+
+    T* operator () () const { return reeval(); }
+  };
+
+  ///
+  // convenientlty get a typed reference from invoke arguments.
+  // example: auto reref = get_ref(this, self, rights, current()->space());
+  template<typename T, typename OSPC>
+  static constexpr Typed_ref<T, OSPC>
+  get_ref(T *obj, L4_obj_ref ref, L4_fpage::Rights r, OSPC *space)
+  { return Typed_ref<T, OSPC>(obj, ref, r, space); }
+
+
   virtual void invoke(L4_obj_ref self, L4_fpage::Rights rights, Syscall_frame *, Utcb *) = 0;
 
   typedef Kobject_iface *Factory_func(Ram_quota *q,
