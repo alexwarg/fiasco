35c6c2a3c5fafb35a546601791f7d2d220f45244
Alexander Warg <alex@wargs.de>
2026-06-25 18:45:15 +0200

    Fix vkey w/o serial (so no vkey...)
    Change-Id: I964bc366335c5233ac405cbcdf51694f3ad28cfa

--- src/kern/vkey_noserial.cc
+++ src/kern/vkey_noserial.cc
@@ -5,16 +5,13 @@ namespace Vkey
 {
 void irq(Irq_base *i) {}
 void set_echo(Echo_type) {}
-void set_echo(Echo_type) {}
 void clear() {}
+void irq(Irq_base *const *) {}
 
 int get()
 {
   return Kconsole::console()->getchar(0);
 }
-
-int check_(int = -1)
-{ return 0; }
 
 }
 
