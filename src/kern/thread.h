e915f4a2c5333553e33c7322760ec4bee9cdd639
Alexander Warg <alex@wargs.de>
2026-08-04 19:47:13 +0200

    Remove deprecated zero syscall flags
    The zero flags as a call are long depreceted, this change removes the
    behavior and changes it to a noop in this case.
    
    Change-Id: I94cc3f009a8306d6a261792ac9e1d8c6684add7b
    Signed-off-by: Alexander Warg <alex@wargs.de>

--- src/kern/thread.h
+++ src/kern/thread.h
@@ -243,14 +243,6 @@ public:
     if (flags & L4_obj_ref::Ipc_send)
       *partner = const_cast<Thread*>(this);
 
-    // FIXME: shall be removed flags == 0 is no-op
-    if (!flags)
-      {
-        *sender = const_cast<Thread*>(this);
-        *partner = const_cast<Thread*>(this);
-        *have_recv = true;
-      }
-
     return *have_recv || ((flags & L4_obj_ref::Ipc_send) && *partner);
   }
 
